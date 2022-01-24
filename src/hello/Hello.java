e68b2ad3c63958d5b8c82fb8221fa8542b838202
Beyza <yildizzbeyzza@gmail.com>
2022-01-24 10:39:30 -0500

    doing final work on hello

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-
-        //hello
+        System.out.println("B25 LEARNING GIT AND GITHUB");
     }
 }
